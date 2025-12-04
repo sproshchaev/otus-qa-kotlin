ce49ae0516897129883ec5b8284fc1aaa871ccfc
sproshchaev <sproshchaev@gmail.com>
2025-12-04 09:28:22 +0500

    add examples

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,21 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()  // Официальный портал плагинов
+        mavenCentral()        // Maven репозиторий
+        google()              // Google (для Android)
+    }
 }
+
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    // Kotlin plugin managed centrally
+    kotlin("jvm") version "2.2.20" apply false
+}
+
 rootProject.name = "otus-qa-kotlin"
+
+// Подключение модулей для многомодульного проекта
 include("m1-lecture-05")
+include("m2-lecture-01")
+include("m2-lecture-01")
