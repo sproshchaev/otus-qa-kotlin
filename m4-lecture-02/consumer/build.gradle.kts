cda9b24a539c519f18b665d5d159b50c8274d4b0
sproshchaev <sproshchaev@gmail.com>
2026-01-22 19:30:26 +0500

    upd

---
+++ m4-lecture-02/consumer/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "ru.otus"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
