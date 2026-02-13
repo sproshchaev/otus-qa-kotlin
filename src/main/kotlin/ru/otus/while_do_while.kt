be9530a28ede73b8c4aea528c503358ded83ae44
sproshchaev <sproshchaev@gmail.com>
2026-02-13 23:07:18 +0500

    add example

---
+++ src/main/kotlin/ru/otus/while_do_while.kt
@@ -0,0 +1,16 @@
+package ru.otus
+
+fun main() {
+    var i = 1
+    while (i <= 3) {
+        println(i)
+        i++
+    }
+
+    var k = 1
+    do {
+        println(k)
+        k++
+    } while (k <= 3)
+
+}
