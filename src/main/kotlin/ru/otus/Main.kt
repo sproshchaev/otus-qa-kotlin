2364bb0bae72616e5d80264279eb842d41ef8f5e
sproshchaev <sproshchaev@gmail.com>
2026-03-26 21:53:51 +0500

    add example

--- src/main/kotlin/ru/otus/Main.kt
+++ src/main/kotlin/ru/otus/Main.kt
@@ -1,5 +1,12 @@
 package ru.otus
 
 fun main() {
-    println("Hello World!")
+
+    // Неизменяемой переменной
+    val pi = 3.14
+
+    // Изменяемая переменная
+    var radius = 1.0
+    radius = 2 * radius
+
 }
